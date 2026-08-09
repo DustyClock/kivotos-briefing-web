db872b610ba2d6fedc96772addb556c882651ab1
DustyClock <dydghdus12@naver.com>
2026-08-10 06:15:04 +0900

    Configure GitHub authentication

--- web/config.js
+++ web/config.js
@@ -1,12 +1,12 @@
 window.KIVOTOS_CONFIG = {
-  appName: "키보토스 브리핑",
-  githubAppClientId: "Iv23ItHWsmz2GmltRVr",
-  githubRepositoryId: "",
-  githubOwner: "DustyClock",
-  privateRepository: "kivotos-briefing-data",
-  allowedGithubUserId: 315081092,
-  dataPath: "data",
-  privateDataPath: "dashboard-data",
-  collectWorkflow: "collect.yml",
-  scheduleWorkflow: "update-schedule.yml"
+    appName: "키보토스 브리핑",
+    githubAppClientId: "Iv23ItHWsmz2GmltRVr",
+    githubRepositoryId: "",
+    githubOwner: "DustyClock",
+    privateRepository: "kivotos-briefing-data",
+    allowedGithubUserId: 315081092,
+    dataPath: "data",
+    privateDataPath: "dashboard-data",
+    collectorWorkflow: "collect.yml",
+    scheduleWorkflow: "update-schedule.yml"
 };
