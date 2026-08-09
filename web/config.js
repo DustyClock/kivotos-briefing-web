bfccae5c43d8385de287ca170560a5f1fbc7e131
DustyClock <dydghdus12@naver.com>
2026-08-10 05:24:37 +0900

    Add Kivotos Briefing dashboard

---
+++ web/config.js
@@ -0,0 +1,12 @@
+window.KIVOTOS_CONFIG = {
+  appName: "키보토스 브리핑",
+  githubAppClientId: "",
+  githubRepositoryId: "",
+  githubOwner: "",
+  privateRepository: "",
+  allowedGithubUserId: 0,
+  dataPath: "data",
+  privateDataPath: "dashboard-data",
+  collectWorkflow: "collect.yml",
+  scheduleWorkflow: "update-schedule.yml"
+};
